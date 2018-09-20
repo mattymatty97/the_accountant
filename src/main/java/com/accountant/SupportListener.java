89470fee1c2d895bfc4d43357b36a90b2a9581b1
mattymatty <33039934+mattymatty97@users.noreply.github.com>
2018-09-20 09:18:38 +0200

    v3.2 - build 3 support update

--- src/main/java/com/accountant/SupportListener.java
+++ src/main/java/com/accountant/SupportListener.java
@@ -12,6 +12,9 @@ import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("Duplicates")
 public class SupportListener extends ListenerAdapter {
     static final long supportID = 428163245753499653L;
@@ -30,28 +33,37 @@ public class SupportListener extends ListenerAdapter {
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         if (event.getUser().getIdLong() == 417349274481721345L)
             if (event.getGuild().getIdLong() != supportID)
-                userUpdate(event.getJDA(), event.getUser());
+                userUpdate(event.getJDA(), event.getUser(),event.getGuild(),true);
     }
 
     @Override
     public void onGuildMemberLeave(GuildMemberLeaveEvent event) {
             if (event.getUser().getIdLong() == 417349274481721345L)
                 if (event.getGuild().getIdLong() != supportID)
-                    userUpdate(event.getJDA(), event.getUser());
+                    userUpdate(event.getJDA(), event.getUser(),event.getGuild(),false);
     }
 
 
-    private void userUpdate(JDA api, User user) {
+    private void userUpdate(JDA api, User user,Guild server,boolean join) {
         Member member = api.getGuildById(supportID).getMemberById(user.getIdLong());
         if (member == null)
             return;
+
+        List<Role> roles = new ArrayList<>(2);
+        if(server.getMembers().stream().map(Member::getUser).map(User::getName).anyMatch(name -> name.equals("Emoji-er")))
+            if(join)
+                roles.add(server.getRoleById(491954024367652867L));
+        if(server.getMembers().stream().map(Member::getUser).map(User::getName).anyMatch(name -> name.equals("RoleGroup")))
+            if(join)
+                roles.add(server.getRoleById(491954204106031104L));
         
         boolean isUser = api.getMutualGuilds(member.getUser()).stream().anyMatch(guild -> guild.getIdLong() != supportID);
 
         boolean hasrole = member.getRoles().contains(botRole);
 
-        if (isUser && !hasrole) {
-            api.getGuildById(supportID).getController().addRolesToMember(member, botRole).reason("guild join").complete();
+        if (join) {
+            roles.add(botRole);
+            api.getGuildById(supportID).getController().addRolesToMember(member, roles).reason("guild join").complete();
         } else if (hasrole && !isUser) {
             api.getGuildById(supportID).getController().removeRolesFromMember(member, botRole).reason("guild leave").complete();
         }
@@ -60,14 +72,14 @@ public class SupportListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
             if (false) {
-                event.getGuild().getMembers().forEach(member -> userUpdate(event.getJDA(), member.getUser()));
+                event.getGuild().getMembers().forEach(member -> userUpdate(event.getJDA(), member.getUser(),member.getGuild(),true));
             }
     }
 
     @Override
     public void onGuildLeave(GuildLeaveEvent event) {
             if (false) {
-                event.getGuild().getMembers().forEach(member -> userUpdate(event.getJDA(), member.getUser()));
+                event.getGuild().getMembers().forEach(member -> userUpdate(event.getJDA(), member.getUser(),member.getGuild(),false));
             }
     }
 
